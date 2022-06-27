263bb36bd4724076142e56e48f8536f84a072072
Kasey Jones <kaseyjones49@gmail.com>
2022-06-27 17:30:08 -0500

    Successfully got the Replies and Comments to connect to admin site

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Comment
+from .serializers import CommentSerializer
 
 # Create your views here.
 
+# Use views.py from cars app for example on most endpoints
+# Instead of get all, add endpoint that allows you to pass in videoid through path
+# and get back all comments with that video id
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def user_cars(request):
+    
--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
     'cars.apps.CarsConfig',  # This app is for example use only
-    'comments',
-    'replies'
+    'comments.apps.CommentsConfig',
+    'replies.apps.RepliesConfig'
 ]
 
 MIDDLEWARE = [
